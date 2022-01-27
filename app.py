6fbe9fd699dcff8654eacdeda1a097471e282e8c
pixelready <bizartist@gmail.com>
2022-01-27 15:19:44 -0800

    add answer route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from markupsafe import re
 from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
@@ -27,4 +28,29 @@ def redirect_to_survey():
 def ask_question(question_id):
     """asks a survey question"""
     current_question = survey.questions[question_id]
-    return render_template("question.html", question=current_question)
+    return render_template(
+        "question.html",
+        question=current_question,
+        question_id=question_id
+    )
+
+
+@app.post('/answer')
+def save_answer():
+    """save question response to responses and if survey done,
+    send user a thank you"""
+    question_id = int(request.form.get('question-id'))
+
+    responses.append(request.form.get('answer'))
+
+    if question_id < len(survey.questions):
+        return redirect(f"questions/{question_id + 1}")
+    else:
+        return redirect('/complete')
+
+
+@app.get('/complete')
+def thank_user():
+    """Render post-survey message"""
+
+    return render_template("completion.html")
