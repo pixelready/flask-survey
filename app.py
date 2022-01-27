c3a4fa18e65ed02387ab4d0bc17443a3febc65e2
pixelready <bizartist@gmail.com>
2022-01-27 11:34:02 -0800

    set up homepage form

--- app.py
+++ app.py
@@ -7,3 +7,22 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.get('/')
+def start_survey():
+    """Shows start survey form"""
+    return render_template("survey_start.html", survey=survey)
+
+
+@app.post('/begin')
+def redirect_to_survey():
+    """Redirects POST request to GET survey display"""
+    return redirect('/questions/0')
+
+
+@app.get('/questions/<int:question_id>')
+def ask_question():
+    """asks a survey question"""
